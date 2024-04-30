79d2100e38a88bc831071b36a3724cfa6aa2f298
jiseongyoo <jsing8@nate.com>
2024-04-30 16:48:07 +0900

    ãDeviceDriver read/write 구현

--- src/main/java/DeviceDriver.java
+++ src/main/java/DeviceDriver.java
@@ -1,18 +1,36 @@
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 /**
  * This class is used by the operating system to interact with the hardware 'FlashMemoryDevice'.
  */
 public class DeviceDriver {
+    public static final int HARDWARE_ACCESS_TRY = 5;
+    public static final byte BYTE_ERASED = (byte) 0xFF;
+    private FlashMemoryDevice hardware;
 
     public DeviceDriver(FlashMemoryDevice hardware) {
-        // TODO: implement this method
+        this.hardware = hardware;
     }
 
-    public byte read(long address) {
-        // TODO: implement this method
-        return -1;
+    public byte read(long address) throws ReadFailException {
+        byte ret = this.hardware.read(address);
+        LocalDateTime previous = LocalDateTime.now();
+
+        for (int i = 1; i < HARDWARE_ACCESS_TRY; i++) {
+            while (ChronoUnit.MILLIS.between(previous, LocalDateTime.now()) < 200){}
+            if (this.hardware.read(address) != ret) {
+                throw new ReadFailException();
+            }
+            previous = LocalDateTime.now();
+        }
+        return ret;
     }
 
-    public void write(long address, byte data) {
-        // TODO: implement this method
+    public void write(long address, byte data) throws WriteFailException {
+        if (this.hardware.read(address) != BYTE_ERASED) {
+            throw new WriteFailException();
+        }
+        this.hardware.write(address, data);
     }
 }
